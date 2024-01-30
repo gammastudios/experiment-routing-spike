eeff0e0a09630300bd7da72cf492bc25f83190d0
Chris Ottinger <chris.ottinger@gammadata.io>
2024-01-31 07:36:59 +1100

    feat(xp-rtr): adding template processing to query string

--- README.md
+++ README.md
@@ -23,26 +23,26 @@ Feature: Service Request Treatment Routing
 Routing service requests to different experimental treatments based on the client making the request.
 
 Background:  
-    Given a service "svc-a" with treatments "t1" and "t2"  
-        And a client "r1" with client-id "1234"  
-        And a client "r2" with client-id "9876"  
-        And client "r1" is assigned to cohort "c1"  
-        And client "r2" is assigned to cohort "c2"  
-        And cohort "c1" is assigned to service treatment "c1"  
-        And cohort "c2" is assigned to service treatment "c2"
+    * Given a service "svc-a" with treatments "t1" and "t2"  
+        - And a client "r1" with client-id "1234"  
+        - And a client "r2" with client-id "9876"  
+        - And client "r1" is assigned to cohort "c1"  
+        - And client "r2" is assigned to cohort "c2"  
+        - And cohort "c1" is assigned to service treatment "c1"  
+        - And cohort "c2" is assigned to service treatment "c2"
 
 Scenario: Service request from "r1" with treatment "t1"  
-    Given I am making a request as client "r1"  
-    When I submit a GET request to "/services/svc-a?client-id=1234"  
-    Then the request is serviced using treatment "t1"
+    * Given I am making a request as client "r1"  
+    * When I submit a GET request to "/services/svc-a?client-id=1234"  
+    * Then the request is serviced using treatment "t1"
 
 Scenario: Service request from "r2" with treatment "t2"  
-    Given I am making a request as client "r2"  
-    When I submit a GET request to "/services/svc-a?client-id=9876"  
-    Then the request is serviced using treatment "t2"
+    * Given I am making a request as client "r2"  
+    * When I submit a GET request to "/services/svc-a?client-id=9876"  
+    * Then the request is serviced using treatment "t2"
 
-Scenario: Request id added as header field
-  Given I make a request as client "r1"
-  When I submit a GET request to "/services/svc-a?client-id=1234"
-  Then a 'X-Request-ID' header field is added to the request response,
-    And the 'X-Request-ID' header field value is a uuid.
+Scenario: Request id added as header field  
+    * Given I make a request as client "r1"  
+    * When I submit a GET request to "/services/svc-a?client-id=1234"  
+    * Then a 'X-Request-ID' header field is added to the request response,  
+    * And the 'X-Request-ID' header field value is a uuid.
--- xp_rtr/app.py
+++ xp_rtr/app.py
@@ -1,87 +1,133 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
-from fastapi import Query
+from fastapi import (
+    FastAPI,
+    Query,
+    HTTPException,
+)
+import os
+from structlog import get_logger
 import yaml
+
+from jinja2 import (
+    Environment,
+    select_autoescape,
+)
 
 from .treatments.t1_treatment import t1_handler
 from .treatments.t2_treatment import t2_handler
 from .treatments.default_treatment import default_handler
-import os
+from fastapi import Request
 
 # service map from a file
-service_mapping_filename = 'client-cohort-map.yml'
+service_mapping_filename = "client-cohort-map.yml"
 
-service_client_map = {} # lookup cohort-name by service-name & client-id 
-service_treatment_map = {} # lookup treatment by service-name & cohort-name
+service_client_map = {}  # lookup cohort-name by service-name & client-id
+service_treatment_map = {}  # lookup treatment by service-name & cohort-name
+treatment_routes = {}  # lookup treatment details by treatment-name
+
+logger = None
+jinja_env = None
 
 
 def load_service_map(file_path):
+    global service_client_map, service_treatment_map, logger
     current_dir = os.path.dirname(os.path.abspath(__file__))
     relative_path = os.path.join(current_dir, file_path)
-    with open(relative_path, 'r') as file:
+    with open(relative_path, "r") as file:
         data = yaml.safe_load(file)
 
     # construct the service client map and service treatment map
     for svc in data:
-        service_name = svc['service-name']
+        service_name = svc["service-name"]
         service_client_map[service_name] = {}
-        for cohort in svc['service-cohorts']:
-            for client_id in cohort['client-ids']:
-                service_client_map[service_name][client_id] = cohort['cohort-name']
+        for cohort in svc["service-cohorts"]:
+            for client_id in cohort["client-ids"]:
+                service_client_map[service_name][client_id] = cohort["cohort-name"]
 
         service_treatment_map[service_name] = {}
-        for exp in svc['service-experiments']:
-            for treatment in exp['treatments']:
-                for cohort_name in treatment['assigned-cohorts']:
-                    service_treatment_map[service_name][cohort_name] = treatment['treatment-name']
+        treatment_routes[service_name] = {}
+        for exp in svc["service-experiments"]:
+            if "default-experiment" in exp and exp["default-experiment"] is True:
+                default_exp = True
+            else:
+                default_exp = False
+            for treatment in exp["treatments"]:
+                treatment_routes[service_name][treatment["treatment-name"]] = treatment[
+                    "treatment-route"
+                ]
+                if (
+                    default_exp is True
+                    and "default-treatment" in treatment
+                    and treatment["default-treatment"] is True
+                ):
+                    treatment_routes[service_name]["default"] = treatment[
+                        "treatment-route"
+                    ]
+                for cohort_name in treatment["assigned-cohorts"]:
+                    service_treatment_map[service_name][cohort_name] = treatment[
+                        "treatment-name"
+                    ]
 
-    print("client_mapping to cohort")
-    print(service_client_map)
-    print(service_treatment_map)
-
-
-    # service.experiment.treatment.cohort
-
-    # select treatment-details
-    # from treatments
-    # where treatment-id = <treatment-id>
-    # | treatment | treatment-details |
-    return data
+    logger.info(
+        "routing config",
+        service_client_map=service_client_map,
+        service_treatment_map=service_treatment_map,
+        treatment_routes=treatment_routes,
+    )
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global service_map
-    service_map = load_service_map(service_mapping_filename)
+    global logger, service_mapping_filename, jinja_env
+    logger = get_logger("xp-rtr")
+    load_service_map(service_mapping_filename)
+    jinja_env = Environment(autoescape=False)
     yield
 
 
-def treatment_lookup(service_name, client_id):
+def treatment_route_lookup(trgt_service: str, client_id: int) -> dict:
     """
-    Look up the treatment name to appliy for a given service and client combination.
-    If treatment cannot be found, then a default experiment.treatment value is returned.
-    e.g. "<svc>.default.default"
-
-
-    Args:
-        service_name (str): Name of the service being requested
-        client_id (str): Client ID to look up
-
-    Returns:
-      str: Treatment name, using the format '<svc>.<experiment>.<treatment>' 
+    Given a target service and client id, return the treatement route details
     """
-    return 'svc-a.exp-a.t1'
+    # get the customer to cohort mapping
+    if trgt_service not in treatment_routes:
+        return None
+    if client_id not in service_client_map[trgt_service]:
+        # return the default treatment for the service
+        return treatment_routes[trgt_service]["default"]
+    cohort = service_client_map[trgt_service][client_id]
+    treatment = service_treatment_map[trgt_service][cohort]
+    return treatment_routes[trgt_service][treatment]
+
 
 app = FastAPI(lifespan=lifespan)
+
+
+def render_jinja_pattern(target_route_pattern: str, client_id: int, query_string=None):
+    uri_template = jinja_env.from_string(target_route_pattern)
+    rendered_string = uri_template.render(client_id=client_id, query_string=query_string)
+    return rendered_string
+
+
 @app.get("/services/{trgt_service}")
-async def handle_service(trgt_service: str, client_id: str = Query(..., alias='client-id', description="Client ID")):
+async def handle_service(
+    trgt_service: str,
+    request: Request,
+    client_id: str = Query(None, alias="client-id", description="Client ID"),
+):
+    if client_id is not None:
+        client_id = int(client_id)
 
     # look up the treatment directly assuming flattened cohort to treatment reference table
-    treatment = treatment_lookup(trgt_service, client_id)
-    print(service_map)
-    match trgt_service:
-        case "svc-a":
-            return t1_handler(client_id)
-        case _:
-            return {"message": f"Handling request for service: {trgt_service}, client ID: {client_id}"}
+    treatment_route = treatment_route_lookup(trgt_service, client_id)
 
+    if treatment_route is None:
+        raise HTTPException(status_code=404, detail="Not Found")
+
+    query_params = request.query_params
+    logger.info("query_params", query_params=query_params)
+    target_uri = render_jinja_pattern(
+        treatment_route["target-pattern"], client_id=client_id, query_string=query_params
+    )
+
+    return f"I would process treatment route: {target_uri}"
